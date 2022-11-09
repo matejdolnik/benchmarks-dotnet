3430b1a1c72d63439928fff31e8713f32f3fc1e2
matej.dolnik <matej.dolnik@thermofisher.com>
2022-11-09 19:19:39 +0100

    sealed class bench marks

---
+++ BenchMarks/SealedClass/BenchMarks.cs
@@ -0,0 +1,35 @@
+﻿
+using BenchmarkDotNet.Attributes;
+
+namespace SealedClass;
+
+[MemoryDiagnoser(false)]
+public class BenchMarks
+{
+    private ClassTypes.BaseClass BaseClass => new ClassTypes.BaseClass();
+    private readonly ClassTypes.SealedClass sealedClass = new ClassTypes.SealedClass();
+    private readonly ClassTypes.OpenClass openClass = new ClassTypes.OpenClass();
+
+    
+    [Benchmark()]
+    public void BaseVoidMethod() => BaseClass.VoidMethod();
+    [Benchmark()]
+    public void OpenVoidMethod() => openClass.VoidMethod();
+    [Benchmark()]
+    public void SealedVoidMethod() => sealedClass.VoidMethod();
+
+    [Benchmark()]
+    public string BaseStringMethod() => BaseClass.StringMethod;
+    [Benchmark()]
+    public string OpenStringMethod() => openClass.StringMethod;
+    [Benchmark()]
+    public string SealedStringMethod() => sealedClass.StringMethod;
+    
+    
+    [Benchmark()]
+    public int BaseIntMethod() => BaseClass.IntMethod;
+    [Benchmark()]
+    public int OpenIntMethod() => openClass.IntMethod;
+    [Benchmark()]
+    public int SealedIntMethod() => sealedClass.IntMethod;
+}
---
+++ BenchMarks/SealedClass/ClassTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace SealedClass;
+
+public class ClassTypes
+{
+    public class BaseClass
+    {
+        public virtual int IntMethod => 55;
+        public virtual string StringMethod => "string";
+        public virtual void VoidMethod() { }
+    }
+
+    public class OpenClass: BaseClass
+    {
+        public override int IntMethod => 155;
+        public override string StringMethod => "alsostring";
+        public override void VoidMethod() { }
+    }
+
+    public sealed class SealedClass : BaseClass
+    {
+        public override int IntMethod => 155;
+        public override string StringMethod => "alsostring";
+        public override void VoidMethod() { }
+    }
+}
